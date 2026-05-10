0a45133e5f41faa0a7a6dcf1b229da0efa3aa5c9
JinwookChei <woook43@gmail.com>
2026-05-10 11:32:20 +0900

    update : README-Header

--- Content/Content/PortfolioLevel.cpp
+++ Content/Content/PortfolioLevel.cpp
@@ -35,52 +35,52 @@ void PortfolioLevel::BeginPlay()
 	//GetEditorCamera()->GetWorldTransform().SetRotation({ 0.0f, 47.0f, -45.0f, 0.0f });
 
 	// Phong Light 2
-	ColorCube* pCube = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
-	pCube->GetWorldTransform().SetScale({ 15.0f, 15.0f, 1.0f, 0.0f });
-	pCube->GetWorldTransform().SetRotation({ 0.0f, 0.0f, 0.0f, 0.0f });
-	pCube->GetWorldTransform().SetPosition({ 0.0f, 0.0f, 0.0f, 1.0f });
-	ColorCube* pCube1 = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
-	pCube1->GetWorldTransform().SetScale({ 15.0f, 15.0f, 1.0f, 0.0f });
-	pCube1->GetWorldTransform().SetRotation({ 90.0f, 0.0f, 0.0f, 0.0f });
-	pCube1->GetWorldTransform().SetPosition({ 0.0f, 7.5f, 7.5f, 1.0f });
-	ColorCube* pCube2 = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
-	pCube2->GetWorldTransform().SetScale({ 15.0f, 15.0f, 1.0f, 0.0f });
-	pCube2->GetWorldTransform().SetRotation({ -90.0f, 0.0f, 0.0f, 0.0f });
-	pCube2->GetWorldTransform().SetPosition({ 0.0f, -7.5f, 7.5f, 1.0f });
-	ColorCube* pCube3 = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
-	pCube3->GetWorldTransform().SetScale({ 15.0f, 15.0f, 1.0f, 0.0f });
-	pCube3->GetWorldTransform().SetRotation({ 0.0f, 90.0f, 0.0f, 0.0f });
-	pCube3->GetWorldTransform().SetPosition({ 7.5f, 0.0f, 7.5f, 1.0f });
+	//ColorCube* pCube = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+	//pCube->GetWorldTransform().SetScale({ 15.0f, 15.0f, 1.0f, 0.0f });
+	//pCube->GetWorldTransform().SetRotation({ 0.0f, 0.0f, 0.0f, 0.0f });
+	//pCube->GetWorldTransform().SetPosition({ 0.0f, 0.0f, 0.0f, 1.0f });
+	//ColorCube* pCube1 = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+	//pCube1->GetWorldTransform().SetScale({ 15.0f, 15.0f, 1.0f, 0.0f });
+	//pCube1->GetWorldTransform().SetRotation({ 90.0f, 0.0f, 0.0f, 0.0f });
+	//pCube1->GetWorldTransform().SetPosition({ 0.0f, 7.5f, 7.5f, 1.0f });
+	//ColorCube* pCube2 = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+	//pCube2->GetWorldTransform().SetScale({ 15.0f, 15.0f, 1.0f, 0.0f });
+	//pCube2->GetWorldTransform().SetRotation({ -90.0f, 0.0f, 0.0f, 0.0f });
+	//pCube2->GetWorldTransform().SetPosition({ 0.0f, -7.5f, 7.5f, 1.0f });
+	//ColorCube* pCube3 = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+	//pCube3->GetWorldTransform().SetScale({ 15.0f, 15.0f, 1.0f, 0.0f });
+	//pCube3->GetWorldTransform().SetRotation({ 0.0f, 90.0f, 0.0f, 0.0f });
+	//pCube3->GetWorldTransform().SetPosition({ 7.5f, 0.0f, 7.5f, 1.0f });
 
-	ColorCube* pCubeA = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
-	pCubeA->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
-	pCubeA->GetWorldTransform().SetPosition({ 0.0f, 5.0f, 0.0f, 1.0f });
-	ColorCube* pCubeB = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
-	pCubeB->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
-	pCubeB->GetWorldTransform().SetPosition({ 0.0f, 5.0f, 0.0f, 1.0f });
-	ColorCube* pCubeC = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
-	pCubeC->GetWorldTransform().SetPosition({ 0.0f, 5.0f, 0.0f, 1.0f });
-	pCubeC->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
-	ColorSphere* pSphereA = SpawnActor<ColorSphere>(E_ACTOR_TYPE::NORMAL);
-	pSphereA->GetWorldTransform().SetPosition({ 0.0f, -5.0f, 0.0f, 1.0f });
-	pSphereA->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
-	ColorSphere* pSphereB = SpawnActor<ColorSphere>(E_ACTOR_TYPE::NORMAL);
-	pSphereB->GetWorldTransform().SetPosition({ 0.0f, -5.0f, 0.0f, 1.0f });
-	pSphereB->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
-	ColorSphere* pSphereC = SpawnActor<ColorSphere>(E_ACTOR_TYPE::NORMAL);
-	pSphereC->GetWorldTransform().SetPosition({ 0.0f, -5.0f, 0.0f, 1.0f });
-	pSphereC->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
-	PointLight* pPointLightA = SpawnLight<PointLight>();
-	pPointLightA->GetWorldTransform().SetPosition({ 0.0f, 0.0f, 5.0f, 1.0f });
-	pPointLightA->GetWorldTransform().SetRotation({ 0.0f, 0.0f, 0.0f, 0.0f });
-	PointLight* pPointLightB = SpawnLight<PointLight>();
-	pPointLightB->GetWorldTransform().SetPosition({ 0.0f, 0.0f, 5.0f, 1.0f });
-	pPointLightB->GetWorldTransform().SetRotation({ 0.0f, 0.0f, 0.0f, 0.0f });
-	PointLight* pPointLightC = SpawnLight<PointLight>();
-	pPointLightC->GetWorldTransform().SetPosition({ 0.0f, 0.0f, 5.0f, 1.0f });
-	pPointLightC->GetWorldTransform().SetRotation({ 0.0f, 0.0f, 0.0f, 0.0f });
-	
-	
+	//ColorCube* pCubeA = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+	//pCubeA->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
+	//pCubeA->GetWorldTransform().SetPosition({ 0.0f, 5.0f, 0.0f, 1.0f });
+	//ColorCube* pCubeB = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+	//pCubeB->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
+	//pCubeB->GetWorldTransform().SetPosition({ 0.0f, 5.0f, 0.0f, 1.0f });
+	//ColorCube* pCubeC = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+	//pCubeC->GetWorldTransform().SetPosition({ 0.0f, 5.0f, 0.0f, 1.0f });
+	//pCubeC->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
+	//ColorSphere* pSphereA = SpawnActor<ColorSphere>(E_ACTOR_TYPE::NORMAL);
+	//pSphereA->GetWorldTransform().SetPosition({ 0.0f, -5.0f, 0.0f, 1.0f });
+	//pSphereA->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
+	//ColorSphere* pSphereB = SpawnActor<ColorSphere>(E_ACTOR_TYPE::NORMAL);
+	//pSphereB->GetWorldTransform().SetPosition({ 0.0f, -5.0f, 0.0f, 1.0f });
+	//pSphereB->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
+	//ColorSphere* pSphereC = SpawnActor<ColorSphere>(E_ACTOR_TYPE::NORMAL);
+	//pSphereC->GetWorldTransform().SetPosition({ 0.0f, -5.0f, 0.0f, 1.0f });
+	//pSphereC->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
+	//PointLight* pPointLightA = SpawnLight<PointLight>();
+	//pPointLightA->GetWorldTransform().SetPosition({ 0.0f, 0.0f, 5.0f, 1.0f });
+	//pPointLightA->GetWorldTransform().SetRotation({ 0.0f, 0.0f, 0.0f, 0.0f });
+	//PointLight* pPointLightB = SpawnLight<PointLight>();
+	//pPointLightB->GetWorldTransform().SetPosition({ 0.0f, 0.0f, 5.0f, 1.0f });
+	//pPointLightB->GetWorldTransform().SetRotation({ 0.0f, 0.0f, 0.0f, 0.0f });
+	//PointLight* pPointLightC = SpawnLight<PointLight>();
+	//pPointLightC->GetWorldTransform().SetPosition({ 0.0f, 0.0f, 5.0f, 1.0f });
+	//pPointLightC->GetWorldTransform().SetRotation({ 0.0f, 0.0f, 0.0f, 0.0f });
+
+
 	// Direction Light
 	//ColorCube* pCube = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
 	//pCube->GetWorldTransform().SetScale({ 10.0f, 10.0f, 3.0f, 0.0f });
@@ -309,7 +309,7 @@ void PortfolioLevel::BeginPlay()
 	//PointLight* pPointLight3 = SpawnLight<PointLight>();
 	//pPointLight3->GetWorldTransform().SetPosition({ 5.8f, 3.4f, 11.0f, 1.0f });
 
-	
+
 	// DebugRendering, Object Picking
 	//DirectionLight* pDirectionLight = SpawnLight<DirectionLight>();
 	//pDirectionLight->GetWorldTransform().SetPosition({ 0.0f, 0.0f, 10.0f, 1.0f });
@@ -330,6 +330,77 @@ void PortfolioLevel::BeginPlay()
 	//Sphere* pSphere5 = SpawnActor<Sphere>(E_ACTOR_TYPE::NORMAL);
 	//pSphere5->GetWorldTransform().SetPosition({ 2.5f, 7.4f, 7.0f, 1.0f });
 	//pSphere5->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
+
+
+	// Phong Light 2
+	float maxR = 50.0f;
+	float halfR = 25.0f;
+	ColorCube* pCube = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+	pCube->GetWorldTransform().SetScale({ maxR, maxR, 1.0f, 0.0f });
+	pCube->GetWorldTransform().SetRotation({ 0.0f, 0.0f, 0.0f, 0.0f });
+	pCube->GetWorldTransform().SetPosition({ 0.0f, 0.0f, 0.0f, 1.0f });
+	ColorCube* pCube1 = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+	pCube1->GetWorldTransform().SetScale({ maxR, maxR, 1.0f, 0.0f });
+	pCube1->GetWorldTransform().SetRotation({ 90.0f, 0.0f, 0.0f, 0.0f });
+	pCube1->GetWorldTransform().SetPosition({ 0.0f, halfR, halfR, 1.0f });
+	ColorCube* pCube2 = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+	pCube2->GetWorldTransform().SetScale({ maxR, maxR, 1.0f, 0.0f });
+	pCube2->GetWorldTransform().SetRotation({ -90.0f, 0.0f, 0.0f, 0.0f });
+	pCube2->GetWorldTransform().SetPosition({ 0.0f, -halfR, halfR, 1.0f });
+	ColorCube* pCube3 = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+	pCube3->GetWorldTransform().SetScale({ maxR, maxR, 1.0f, 0.0f });
+	pCube3->GetWorldTransform().SetRotation({ 0.0f, 90.0f, 0.0f, 0.0f });
+	pCube3->GetWorldTransform().SetPosition({ halfR, 0.0f, halfR, 1.0f });
+	for (int i = 0; i < 100; ++i)
+	{
+		// -25.0f ~ 25.0f 사이의 랜덤 좌표 계산
+		float randomX = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * maxR - halfR;
+		float randomY = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * maxR - halfR;
+		float randomZ = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * maxR;
+
+		// 1. 액터 스폰
+		ColorCube* pNewCube = SpawnActor<ColorCube>(E_ACTOR_TYPE::NORMAL);
+		if (pNewCube)
+		{
+			pNewCube->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
+			pNewCube->GetWorldTransform().SetPosition({ randomX, randomY, randomZ, 1.0f });
+		}
+	}
+
+	for (int i = 0; i < 100; ++i)
+	{
+		// -25.0f ~ 25.0f 사이의 랜덤 좌표 계산
+		float randomX = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * maxR - halfR;
+		float randomY = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * maxR - halfR;
+		float randomZ = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * maxR;
+
+		// 1. 액터 스폰
+		ColorSphere* pNewSphere = SpawnActor<ColorSphere>(E_ACTOR_TYPE::NORMAL);
+		if (pNewSphere)
+		{
+			pNewSphere->GetWorldTransform().SetScale({ 3.0f, 3.0f, 3.0f, 0.0f });
+			pNewSphere->GetWorldTransform().SetPosition({ randomX, randomY, randomZ, 1.0f });
+		}
+	}
+	for (int i = 0; i < 100; ++i)
+	{
+		float randomX = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * maxR - halfR;
+		float randomY = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * maxR - halfR;
+		float randomZ = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * maxR;
+		float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+		float g = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+		float b = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+		PointLight* pNewLight = SpawnLight<PointLight>();
+		if (pNewLight)
+		{
+			pNewLight->SetDiffuseColorForEditor({ r, g, b, 1.0f });
+			pNewLight->SetLightRange(maxR);
+			pNewLight->GetWorldTransform().SetPosition({ randomX, randomY, randomZ, 1.0f });
+			pNewLight->GetWorldTransform().SetRotation({ 0.0f, 0.0f, 0.0f, 0.0f });
+		}
+	}
+	GetEditorCamera()->GetWorldTransform().SetPosition({ -40.0f, 0.0f, 5.0f, 1.0f });
+	GetEditorCamera()->GetWorldTransform().SetRotation({ 0.0f, -20.0f, 0.0f, 1.0f });
 }
 
 void PortfolioLevel::Tick(double deltaTime)
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# OogieEngine
+<div align="center">
+
+  <!-- 엔진 구동 화면 스크린샷이나 로고 이미지를 넣어주세요 -->
+  <img src="./Preview/OogieEngine.png" alt="OogieEngine Main Screen" width="700"/>
+
+  <br/>
+
+  # OogieEngine - DirectX11 3D Graphics Engine
+
+  **고성능 렌더링과 시스템 최적화를 고려하여 C++ 및 DirectX 11 기반으로 자체 제작한 3D 그래픽스 엔진입니다.**
+
+</div>
